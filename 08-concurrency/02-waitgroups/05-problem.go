9a40d6f242d9dbaecc0b6dd4cdf03cb4a50b8d89
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-29 16:24:05 +0530

    assignment

---
+++ 08-concurrency/02-waitgroups/05-problem.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	for no := 2; no <= 100; no++ {
+		printIfPrime(no)
+	}
+	fmt.Println("Done")
+}
+
+func printIfPrime(no int) {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return
+		}
+	}
+	fmt.Println("Prime No :", no)
+}
